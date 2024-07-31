b3415d7da9e4e551d2aba4a47e3b77cd80356537
Rohit Verma <rv17837@outlook.com>
2024-07-31 20:44:00 +0530

    cleanup: remove old error handling class and viewmodel for auth

--- app/src/main/java/com/app/picknotes/utils/TextFieldErrors.kt
+++
@@ -1,7 +0,0 @@
-package com.app.picknotes.utils
-
-data class TextFieldErrors(
-    val usernameError: ValidationResult = ValidationResult(successful = true),
-    val emailError: ValidationResult = ValidationResult(successful = true),
-    val passwordError: ValidationResult = ValidationResult(successful = true)
-)
